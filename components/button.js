2cfcb6a22f0875d75b7e7d849db5e8d3dd0a1a9c
beteab <beteabbaynessagne548@gmail.com>
2025-03-01 12:18:51 -0800

    styling the custom primary buttons with presable

--- components/button.js
+++ components/button.js
@@ -1,10 +1,35 @@
-import { Text, View, StyleSheet } from "react-native";
-
+import { Text, View, StyleSheet, Pressable } from "react-native";
+function handlePress() {
+  console.log("pressed");
+}
 export default function PrimaryButton({ children }) {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={style.buttonOuterContainer}>
+      <Pressable
+        style={style.buttonInnerContainer}
+        onPress={handlePress}
+        android_ripple={{ color: "@72063f" }}
+      >
+        <Text style={style.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  buttonOuterContainer: {
+    overflow: "hidden",
+    borderRadius: 28,
+    width: "80%",
+    margin: 4,
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 18,
+    paddingHorizontal: 26,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+});
--- screens/gameScreen.js
+++ screens/gameScreen.js
@@ -6,8 +6,8 @@ export default function GameScreen() {
       <TextInput
         style={style.numberInput}
         keyboardType="number-pad"
-        autoCapitalize="false"
-        autoCorrect="false"
+        autoCapitalize={false}
+        autoCorrect={false}
         maxLength={2}
       />
       <PrimaryButton>Reset</PrimaryButton>
@@ -19,17 +19,18 @@ const style = StyleSheet.create({
   container: {
     marginTop: "60",
     backgroundColor: "purple",
-    marginHorizontal: 24,
+    marginHorizontal: 14,
     elevation: 6,
     borderRadius: 10,
+    alignItems:'center'
   },
   numberInput: {
-    color: "#dd52f",
-    borderBottomColor: "#dd52f",
-    marginVertical: 8,
+    color: "orange",
+    borderBottomColor: "orange",
+    marginVertical: 6,
     width: 50,
     textAlign: "center",
-    fontSize: 32,
+    fontSize: 26,
     height: 50,
     fontWeight: "bold",
     borderBottomWidth: 2,
