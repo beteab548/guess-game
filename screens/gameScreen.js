f53447354a7b1b743065b057f4de1263ffe85ba4
beteab <beteabbaynessagne548@gmail.com>
2025-03-01 09:28:10 -0800

    styling the textinput

--- screens/gameScreen.js
+++ screens/gameScreen.js
@@ -3,7 +3,13 @@ import PrimaryButton from "../components/button";
 export default function GameScreen() {
   return (
     <View style={style.container}>
-      <TextInput />
+      <TextInput
+        style={style.numberInput}
+        keyboardType="number-pad"
+        autoCapitalize="false"
+        autoCorrect="false"
+        maxLength={2}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -16,5 +22,16 @@ const style = StyleSheet.create({
     marginHorizontal: 24,
     elevation: 6,
     borderRadius: 10,
+  },
+  numberInput: {
+    color: "#dd52f",
+    borderBottomColor: "#dd52f",
+    marginVertical: 8,
+    width: 50,
+    textAlign: "center",
+    fontSize: 32,
+    height: 50,
+    fontWeight: "bold",
+    borderBottomWidth: 2,
   },
 });
